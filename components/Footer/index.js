cc8e6c54ab7c102a90671a93b5ef0c001f5bf78b
gabrielclformiga <gabrielclformiga@gmail.com>
2020-12-16 22:06:11 -0300

    new adjustments.

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="bg-gray-700 p-4">
       <div className="container mx-auto text-center text-white font-bold">
         Projeto desenvolvido por: {' '}
-        <div>Gabriel Cortez</div> / {' '}
+        <span>Gabriel Cortez</span> / {' '}
         <a className="hover:underline" href="https://github.com/gabrielclformiga">Github</a>
         <div className="mt-2">
           <img className="inline p-4" src="/logo_semana_fsm.png" />
